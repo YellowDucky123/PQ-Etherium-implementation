725a899715fa9de5b96d654e581e6a76de3fc1bc
kelvin <kelvinjyoga@gmail.com>
2025-07-05 18:15:39 +1000

    lib.h added

---
+++ src/lib.h
@@ -0,0 +1,7 @@
+#pragma once
+
+constexpr int MESSAGE_LENGTH = 32;
+
+constexpr uint8_t TWEAK_SEPARATOR_FOR_MESSAGE_HASH = 0x02;
+constexpr uint8_t TWEAK_SEPARATOR_FOR_TREE_HASH = 0x01;
+constexpr uint8_t TWEAK_SEPARATOR_FOR_CHAIN_HASH = 0x00;
