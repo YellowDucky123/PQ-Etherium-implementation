296434ed6afe9dad89d70b39ce6f2912da0c7d76
kelvin <kelvinjyoga@gmail.com>
2025-08-01 14:48:50 +1000

    fix

---
+++ src/params.hpp
@@ -0,0 +1,7 @@
+struct Params {
+    static constexpr unsigned int MESSAGE_LENGTH = 32;
+    
+    static constexpr unsigned int TWEAK_SEPARATOR_FOR_MESSAGE_HASH = 0x02;
+    static constexpr unsigned int TWEAK_SEPARATOR_FOR_TREE_HASH = 0x01;
+    static constexpr unsigned int TWEAK_SEPARATOR_FOR_CHAIN_HASH = 0x00;
+};
--- src/symmetric/message_hash.hpp
+++ src/symmetric/message_hash.hpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include "../params.hpp"
 
 /// class to model a hash function used for message hashing.
 ///
@@ -10,6 +11,7 @@
 /// Note that BASE must be at most 2^8, as we encode chunks as u8.
 template <typename Randomness, typename Parameter>
 class MessageHash {
+    using MESSAGE_LENGTH = Params::MESSAGE_LENGTH;
     // number of entries in a hash
     unsigned int DIMENSION;
 
@@ -19,12 +21,8 @@ public:
     // Generates a random domain element.
     virtual Randomness rand(Rng rng) = 0;
 
-    virtual vector<uint8_t> apply(
-        Parameter parameter,
-        uint32_t epoch,
-        Randomness randomness,
-        array<uint8_t, N> message
-    ) = 0;
+    virtual vector<uint8_t> apply(Parameter parameter, uint32_t epoch, Randomness randomness,
+        array<uint8_t, MESSAGE_LENGTH> message) = 0;
 
     virtual internal_consistency_check() = 0;   
 };
