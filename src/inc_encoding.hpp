312f2c70d6833b621cd0239a07907247a6f42436
SuWingTang <su.w.tang@student.unsw.edu.au>
2025-08-02 11:34:59 +1000

    still wip

--- src/inc_encoding.hpp
+++ src/inc_encoding.hpp
@@ -7,24 +7,30 @@
 
 using namespace std;
 
-template<typename Parameter, typename Randomness>
-class IncomparableEncoding {
+class IncomparableEncoding
+{
 public:
+	using Parameter = typename MessageHash::Parameter;
+	using Randomness = typename MessageHash::Randomness;
 
 	int DIMENSION;
 	int MAX_SIZE;
 	int BASE;
 
-	IncomparableEncoding(int DIMENSION, int MAX_SIZE, int BASE) {
+	IncomparableEncoding(MessageHash MH, int DIMENSION, int MAX_SIZE, int BASE)
+	{
 		this->DIMENSION = DIMENSION;
 		this->MAX_SIZE = MAX_SIZE;
 		this->BASE = BASE;
 	}
 
-	virtual int Rand(std::random_device& rng) = 0;
+	// is this right????
+	virtual int Rand(Rng rng)
+	{
+		return MessageHash.rand(rng);
+	}
 
 	virtual tuple<vector<uint8_t>, int> encode(Parameter parameter, vector<uint8_t>, Randomness randomness, int epoch) = 0;
 
 	virtual void internal_consistency_check() = 0;
-
-};
+}
