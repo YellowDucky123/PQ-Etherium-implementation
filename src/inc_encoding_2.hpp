1683082017d816d85e05a6d65f00ed5086c21d65
SuWingTang <su.w.tang@student.unsw.edu.au>
2025-08-03 12:20:21 +1000

    fixed typos and reduced redunancy

--- src/inc_encoding_2.hpp
+++ src/inc_encoding_2.hpp
@@ -5,11 +5,8 @@
 #include "serializable.hpp"
 
 template <typename T>
-concept IncomparableEncoding = requires {
+concept IncomparableEncoding = Serializable<typename T::Parameter> && Serializable<typename T::Randomness> && requires {
         typename T::Parameter;
-        typename T::Randomness;
-} && Serializable<typename T::Parameter> && Serializable<typename T::Randomness> && requires {
-        typename T::Paramter;
         typename T::Randomness;
         { T::DIMENSION } -> std::convertible_to<std::size_t>;
         { T::MAX_TRIES } -> std::convertible_to<std::size_t>;
