5c55ada9e81b7bf3053a1026cfe29265414d032c
kelvin <kelvinjyoga@gmail.com>
2025-08-03 22:19:49 +1000

    added signature.hpp

---
+++ src/signature.hpp
@@ -0,0 +1,15 @@
+#include <cstdint>
+#include <tuple>
+
+template <typename PublicKey, typename SecretKey, typename Signature>
+struct SignatureScheme {
+    const uint64_t LIFETIME;
+
+    virtual tuple<PublicKey, SecretKey> key_gen(uint activation_epoch, uint num_active_epochs) = 0;
+
+    virtual Signature sign(const SecretKey& sk, uint32_t epoch, const std::vector<uint8_t> msg) = 0;
+
+    virtual bool verify(const PublicKey& pk, uint32_t epoch, const std::vector<uint8_t> msg, const Signature& sig) = 0;
+
+    virtual void internal_consistency_check() = 0;
+};
