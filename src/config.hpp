414b97754793cfc6ca0e76014c7d484cb3e669fe
SuWingTang <su.w.tang@student.unsw.edu.au>
2025-08-03 11:20:29 +1000

    defined global constant values

--- src/config.hpp
+++ src/config.hpp
@@ -3,9 +3,9 @@
 #ifndef CONFIG_HPP
 #define CONFIG_HPP
 
-inline constexpr uint8_t MESSAGE_LENGTH;
-inline constexpr uint8_t TWEAK_SEPARATOR_FOR_MESSAGE_HASH;
-inline constexpr uint8_t TWEAK_SEPARATOR_FOR_TREE_HASH;
-inline constexpr uint8_t TWEAK_SEPARATOR_FOR_CHAIN_HASH;
+inline constexpr uint8_t MESSAGE_LENGTH = 32;
+inline constexpr uint8_t TWEAK_SEPARATOR_FOR_MESSAGE_HASH = 0x02;
+inline constexpr uint8_t TWEAK_SEPARATOR_FOR_TREE_HASH = 0x01;
+inline constexpr uint8_t TWEAK_SEPARATOR_FOR_CHAIN_HASH = 0x00;
 
 #endif
