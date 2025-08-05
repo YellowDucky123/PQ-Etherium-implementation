251273d1321c529aa31f704c79c7dc5b27ba1df7
SuWingTang <su.w.tang@student.unsw.edu.au>
2025-08-05 16:58:23 +1000

    random  module for use

---
+++ src/rand_range.hpp
@@ -0,0 +1,33 @@
+#include <openssl/rand.h>
+#include <array>
+
+class CryptoRng
+{
+public:
+      // Generate cryptographically secure random bytes
+      static void fill_bytes(void *buffer, size_t len)
+      {
+            if (RAND_bytes(static_cast<unsigned char *>(buffer), static_cast<int>(len)) != 1)
+            {
+                  throw std::runtime_error("Failed to generate cryptographically secure random bytes");
+            }
+      }
+
+      // Generat a random value of type T
+      template <typename T>
+      static T generate()
+      {
+            T value;
+            fill_bytes(&value, sizeof(T));
+            return value;
+      }
+
+      // Generate random array
+      template <typename T, size_t N>
+      static std::array<T, N> generate_array()
+      {
+            std::array<T, N> arr;
+            fill_bytes(arr.data(), sizeof(arr));
+            return arr;
+      }
+};
