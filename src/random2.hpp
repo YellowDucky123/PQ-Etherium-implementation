81ecdf20c105de540c299f60786c065e080b0523
tsw21 <73005919+SuTang21@users.noreply.github.com>
2025-08-12 18:08:25 +1000

    Create random2.hpp

---
+++ src/random2.hpp
@@ -0,0 +1,48 @@
+#pragma once
+
+#include <openssl/rand.h>
+#include <array>
+#include <type_traits>
+#include <cstdint>
+#include <vector>
+
+struct Random
+{
+      // Generate cryptographically secure random bytes
+      static void fill_bytes(void *buffer, size_t len)
+      {
+            // Return value for success in RAND_bytes() = 1, otherwise
+            // it is -1 if not produced securely and 0 on other errors.
+            if (RAND_bytes(static_cast<unsigned char *>(buffer), static_cast<int>(len)) != 1)
+            {
+                  throw std::runtime_error("Failed to generate cryptographically secure random bytes");
+            }
+      }
+
+      // Generate randomness for arrays
+      template <typename T, size_t N>
+      static std::array<T, N> generate_array()
+      {
+            std::array<T, N> arr;
+            fill_bytes(arr.data(), sizeof(arr));
+            return arr;
+      }
+
+      // Generate randomness for vectors
+      template <typename T>
+      static std::vector<T> generate_vector(size_t size)
+      {
+            std::vector<T> vec(size);
+            fill_bytes(vec.data(), vec.size() * sizeof(T));
+            return vec;
+      }
+
+      // Generate randomness values types
+      template <typename T>
+      static T generate()
+      {
+            T value;
+            fill_bytes(&value, sizeof(T));
+            return value;
+      }
+};
