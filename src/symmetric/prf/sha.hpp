9e1b47ffe0de9375caea3e52d9a579a8c7a5976e
YellowDucky123 <135637568+YellowDucky123@users.noreply.github.com>
2025-08-12 17:39:21 +1000

    Update sha.hpp

--- src/symmetric/prf/sha.hpp
+++ src/symmetric/prf/sha.hpp
@@ -3,17 +3,14 @@
 
 constexpr unsigned int KEY_LENGTH = 32;
 
-class SHA256PRF : public PseudoRandom<std::vector<uint8_t>, std::vector<uint8_t>>
+struct SHA256PRF : public PseudoRandom<std::vector<uint8_t>, std::vector<uint8_t>>
 {
     using Key = std::vector<uint8_t>;
     using Output = std::vector<uint8_t>;
 
     const unsigned int OUTPUT_LENGTH;
 
-public:
-    SHA256PRF(unsigned int _OUTPUT_LENGTH_) : OUTPUT_LENGTH(_OUTPUT_LENGTH_)
-    {
-    }
+    SHA256PRF(unsigned int _OUTPUT_LENGTH_) : OUTPUT_LENGTH(_OUTPUT_LENGTH_) {}
 
     Key key_gen() override;
 
